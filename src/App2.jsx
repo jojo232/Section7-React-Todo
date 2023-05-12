71a27ea0b12fb90a3a7d99b16d84e7574529308d
Yuya-Sasaki <65487059+jojo232@users.noreply.github.com>
2023-05-12 14:28:33 +0900

    Update App2.jsx

--- src/App2.jsx
+++ src/App2.jsx
@@ -39,3 +39,52 @@ export const App = () => {
 
 //　3つに分かれるのでdivを3つ書いていく
 // JSXで学んだように1つのタグで囲んでおく必要がある。
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+import { useState } from "react";
+import "./styles.css";
+
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["ううう"]);
+
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOリスト" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了リスト</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
